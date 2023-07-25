b59e10232cf141755ed436f505e99cb952f77009
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-07-26 00:57:57 +0600

    Taks 05

--- task-5.js
+++ task-5.js
@@ -1,1 +1,25 @@
 // Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+
+const calculator = (num1, num2, operator) => {
+  let result;
+
+  switch (operator) {
+    case "+":
+      return (result = num1 + num2);
+
+    case "-":
+      return (result = num1 - num2);
+
+    case "*":
+      return (result = num1 * num2);
+
+    case "/":
+      return (result = num1 / num2);
+
+    default:
+      return console.log("Invalid operator");
+  }
+};
+
+// For testing purposes
+console.log(calculator(5, 2, "/"));
