de3b8480c44f5b30f58604ac00eb295246afdd9e
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-07-25 15:59:07 +0600

    All files and questions initialized

---
+++ task-1.js
@@ -0,0 +1,3 @@
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+// Example Input: "hello world" Example Output: "dlrow olleh"
---
+++ task-8.js
@@ -0,0 +1,1 @@
+// Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
