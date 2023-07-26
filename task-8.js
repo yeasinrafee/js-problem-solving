2019eb9fabbac03b1c8b649bc51881865be40f69
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-07-26 21:33:49 +0600

    Task 08

--- task-8.js
+++ task-8.js
@@ -1,1 +1,28 @@
 // Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
+
+const findSecondSmallestNumber = (arr) => {
+  if (arr.length < 2) {
+    return arr[0];
+  }
+
+  let smallestNumber = Infinity;
+  let secondSmallestNumber = Infinity;
+
+  for (const num of arr) {
+    if (num < smallestNumber) {
+      secondSmallestNumber = smallestNumber;
+      smallestNumber = num;
+    } else if (num < secondSmallestNumber && num != smallestNumber) {
+      secondSmallestNumber = num;
+    }
+  }
+
+  if (secondSmallestNumber === Infinity) {
+    return null;
+  }
+
+  return secondSmallestNumber;
+};
+
+// For testing purposes
+console.log(findSecondSmallestNumber([4, 7, 3, 1, 8, 9, 2]));
