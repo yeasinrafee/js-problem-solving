1dd0bf12c7c6f789a76776d4a1db37da24ed1e1c
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-07-26 20:58:59 +0600

    task 06

--- task-6.js
+++ task-6.js
@@ -1,1 +1,22 @@
 // Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+const randomPassword = (len) => {
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const specialChar = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+  const numbers = "0123456789";
+
+  const allChar = upperCase + lowerCase + numbers + specialChar;
+
+  let pass = "";
+
+  for (let i = 0; i < len; i++) {
+    const randIndex = Math.floor(Math.random() * allChar.length);
+    pass += allChar[randIndex];
+  }
+
+  return pass;
+};
+
+// For testing purposes
+console.log(randomPassword(8));
